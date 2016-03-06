118a3d74c3105fdce2456f2828667c90ad1b6e18
Alex Bates <16batesa@gmail.com>
2016-03-06 18:46:02 +0000

    SSL?

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict'
 
+const fs = require('fs')
 const express = require('express')
 const jimp = require('jimp')
+const os = require('os')
+const https = require('http')
+const chalk = require('chalk')
 
 let app = express()
 
@@ -20,6 +24,16 @@ app.get('*', function(req, res) {
 	res.sendFile(__dirname + '/public/404.html')
 })
 
-app.listen(3000, function() {
-	console.log('OpenSprites Image Hosting is listening on port 3000!')
-})
+// ssl for opensprites.org *only*
+if(os.hostname() == 'opensprites.org') {
+	serve.createServer({
+		key: fs.readFileSync('/home/admin/conf/web/ssl.opensprites.org.key'),
+		cert: fs.readFileSync('/home/admin/conf/web/ssl.opensprites.org.crt')
+	}, app).listen(3000, function() {
+		console.log('[	 ] ' + chalk.blue('Listening ' + chalk.underline('(ssl mode enabled!) ') + '...'))
+	})
+} else {
+	app.listen(function() {
+		console.log('[	 ] ' + chalk.blue('Listening...'))
+	})
+}
