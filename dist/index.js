354cba9d82d0ed2357d95f49e4b7e26da83ccc58
Alex Bates <16batesa@gmail.com>
2016-03-06 14:24:31 +0000

    merge

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,2 @@
-"use strict";
+'use strict';require('./upload.js');
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
